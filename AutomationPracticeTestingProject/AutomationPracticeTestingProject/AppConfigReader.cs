d9c38e082ffeab5f6e29f2628a45c387162572fc
SarkerJ <isarkerj@gmail.com>
2021-02-16 17:34:22 +0000

    initial commit with correct extensions and configreader

---
+++ AutomationPracticeTestingProject/AutomationPracticeTestingProject/AppConfigReader.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AutomationPracticeTestingProject
+{
+    public static class AppConfigReader
+    {
+        //public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
+        //public static readonly string SigninPageUrl = ConfigurationManager.AppSettings["signinpage_url"];
+
+        //.NET core doesnt read configreader.... so use the code below instead 
+        public static string BaseUrl { get; }
+        public static string SigninPageUrl { get; }
+        public static string DressesPageUrl { get; }
+
+        static AppConfigReader()
+        {
+            var configBuilder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", false, true);
+            var baseSettingSection = configBuilder.Build().GetSection("base_settings");
+            BaseUrl = baseSettingSection.GetSection("base_url").Value;
+            SigninPageUrl = baseSettingSection.GetSection("signinpage_url").Value;
+            DressesPageUrl = baseSettingSection.GetSection("dressespage_url").Value;
+        }
+    }
+}
