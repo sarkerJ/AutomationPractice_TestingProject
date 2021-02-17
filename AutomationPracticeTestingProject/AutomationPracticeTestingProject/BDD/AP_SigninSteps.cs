c0f48987a7e16ee71f90359f20b41bd418a63b29
SarkerJ <isarkerj@gmail.com>
2021-02-17 09:56:08 +0000

    Added signin feature with passing tests

---
+++ AutomationPracticeTestingProject/AutomationPracticeTestingProject/BDD/AP_SigninSteps.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace AutomationPracticeTestingProject.BDD
+{
+    [Binding]
+    public class AP_SigninSteps
+    {
+        public AP_Website<ChromeDriver> AP_Website;
+
+        [BeforeScenario("@Signin")]
+        public void Method()
+        {
+            AP_Website = new AP_Website<ChromeDriver>();
+        }
+
+        [Given(@"I am on the signin page")]
+        public void GivenIAmOnTheSigninPage()
+        {
+            AP_Website.AP_SigninPage.VisitSignInPage();
+        }
+
+        [Given(@"I have entered an  (.*) and a (.*)")]
+        public void GivenIHaveEnteredAnAndA(string email, string password)
+        {
+            AP_Website.AP_SigninPage.SubmitEmailAndPassword(email, password);
+        }
+
+        [When(@"I click the sign in button")]
+        public void WhenIClickTheSignInButton()
+        {
+            AP_Website.AP_SigninPage.ClickSubmit();
+        }
+
+        [Then(@"I should see an alert containing the error message ""(.*)""")]
+        public void ThenIShouldSeeAnAlertContainingTheErrorMessage(string expected)
+        {
+            Thread.Sleep(2000);
+            Assert.That(AP_Website.AP_SigninPage.GetErrorMessage().Contains(expected));
+        }
+
+        [Then(@"I should see a page with the title ""(.*)""")]
+        public void ThenIShouldSeeAPageWithTheTitle(string expected)
+        {
+            Thread.Sleep(3000);
+            Assert.That(AP_Website.SeleniumDriver.Title, Is.EqualTo(expected));
+        }
+
+
+        [AfterScenario("@Signin")]
+        public void DisposeWebDriver()
+        {
+            AP_Website.SeleniumDriver.Quit();
+            AP_Website.SeleniumDriver.Dispose();
+        }
+    }
+}
---
+++ AutomationPracticeTestingProject/AutomationPracticeTestingProject/BDD/Signin.feature
@@ -0,0 +1,38 @@
+﻿Feature: AP_Signin
+	Provides the user with the ability to sign in by entering their email and password
+
+
+
+@Signin
+Scenario Outline: Password too short
+	Given I am on the signin page
+	And I have entered an  <email> and a <password>
+	When I click the sign in button
+	Then I should see an alert containing the error message "Invalid password"
+	Examples: 
+	| email                  | password |
+	| testingemail@gmail.com | 1234     |
+	| testingemail@gmail.com | wiz      |
+	| testingemail@gmail.com | sick     |
+
+
+@Signin
+Scenario Outline: Invalid email
+	Given I am on the signin page
+	And I have entered an  <email> and a <password>
+	When I click the sign in button
+	Then I should see an alert containing the error message "Invalid email address"
+	Examples: 
+	| email | password     |
+	| hi    | Password123! |
+	| bye   | Password123! |
+
+@Signin
+Scenario Outline: Access account page
+	Given I am on the signin page
+	And I have entered an  <email> and a <password>
+	When I click the sign in button
+	Then I should see a page with the title "My account - My Store"
+	Examples: 
+	| email                  | password     |
+	| testingemail@gmail.com | Password123! |
